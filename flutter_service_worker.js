9fbc5119862daa7e73d56e2a891b354495b41b4e
Tony <tonynguyen0925@gmail.com>
2020-04-21 00:24:31 -0700

    html build update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {
 "icons/ms-icon-70x70.png": "d3ac0a1113eb9e4c0454c06dae10ab5d",
 "index.html": "8bdb57efb112e4e694a65630cd1b5fad",
 "/": "8bdb57efb112e4e694a65630cd1b5fad",
-"main.dart.js": "a00008b702246c633bb345d438cc2f0f"
+"main.dart.js": "e76955124e780336283e99c8cd5c4daa"
 };
 
 self.addEventListener('activate', function (event) {
